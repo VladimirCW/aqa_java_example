ed18053924ee17724ada1189c73f15882f40af54
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2020-05-08 17:30:23 +0300

    added Jenkinsfile

--- src/test/java/ui/FirstTest.java
+++ src/test/java/ui/FirstTest.java
@@ -7,6 +7,7 @@ public class FirstTest {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://google.com");
         driver.manage().window().maximize();
